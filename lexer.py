428e6bd299762f810eefa6c6b6757ad532bbd980
MH.Meraji <mh.meraji12@gmail.com>
2022-03-11 14:44:15 +0330

    Complete lexer class + add main test file

--- lexer.py
+++ lexer.py
@@ -1,3 +1,4 @@
+from re import T
 from ply import lex
 
 class Lexer:
@@ -26,3 +27,27 @@ class Lexer:
     t_IF = r'if'
     t_WHILE = r'while'
     t_PRINT = r'print'
+
+    def t_INTEGER(self, t):
+        r'[-|+]?(\d+)'
+        t.value = int(t.value)
+        return t 
+
+
+    # Define a rule so we can track line numbers
+    def t_newline(self, t):
+        r'\n+'
+        t.lexer.lineno += len(t.value)
+
+    # A string containing ignored characters (spaces and tabs)
+    t_ignore  = '\n \t'
+
+    # Error handling rule
+    def t_error(self, t):
+        print("Illegal character '%s'" % t.value[0])
+        raise Exception('Error For Value, ', t.value, 'With The Type, ', t.type)
+
+    # Build Lexer Class
+    def build(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+        return self.lexer
